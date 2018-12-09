956fa19e877ea9c37942a41d3f69ae71b2ce8445
denny <soulkillty@163.com>
2018-12-09 17:51:26 +0800

    init project

---
+++ sargeras-boot/src/main/java/cn/com/denny/sargeras/boot/SargerasBootApplication.java
@@ -0,0 +1,12 @@
+package cn.com.denny.sargeras.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SargerasBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SargerasBootApplication.class, args);
+    }
+}
